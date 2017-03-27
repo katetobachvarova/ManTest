474b9aa337f470804baa725364ea5341d09fdf49
k.petrova <k.petrova@PC-1227>
2017-03-27 11:16:59 +0300

    ADD:ErrorPage

---
+++ ManTestAppWebForms/Views/ErrorPage.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ErrorPage.aspx.cs" Inherits="ManTestAppWebForms.Views.ErrorPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Error:</h2>
+    <p></p>
+    <asp:Label ID="FriendlyErrorMsg" runat="server" Text="Label" Font-Size="Large" Style="color: red"></asp:Label>
+</asp:Content>
---
+++ ManTestAppWebForms/Views/ErrorPage.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManTestAppWebForms.Views
+{
+    public partial class ErrorPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string generalErrorMsg = "A problem has occurred on this web site. Please try again. " +
+               "If this error continues, please contact support.";
+            FriendlyErrorMsg.Text = generalErrorMsg;
+        }
+    }
+}
